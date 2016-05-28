2f99f81fd18b1b737771003889aa6f09a4a80654
jacean <jaceanwan@gmail.com>
2016-05-28 13:26:08 +0800

    init

---
+++ ChatBot/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ChatBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(12, 28);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(232, 327);
+            this.OutputBox.TabIndex = 0;
+            this.OutputBox.Text = "Give you a little comfort\r\n\r\n";
+            this.OutputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 368);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(153, 21);
+            this.InputBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "I know you\'re lonely";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 401);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.OutputBox);
+            this.Name = "Form1";
+            this.Text = "ChatBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChatBot/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syn.Bot;
+using Syn.EmotionML;
+using System.IO;
+
+namespace ChatBot
+{
+    public partial class Form1 : Form
+    {
+        public SynBot Chatbot;
+        public Form1()
+        {
+            InitializeComponent();
+            Chatbot = new SynBot();
+            Chatbot.PackageManager.LoadFromString(File.ReadAllText("chatBot.simlpk"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = Chatbot.Chat(InputBox.Text);
+            var time = DateTime.Now.ToString("yy/MM/dd-HH:mm:ss");
+
+            Emotion emo=result.UserEmotion;
+            OutputBox.Text = string.Format("{2}    -*-*-{3}-*-*- \r\nUser: {0}\r\nBot: {1}\r\n\r\n", InputBox.Text, result.BotMessage, OutputBox.Text,time);
+            
+            InputBox.Text = string.Empty;
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
